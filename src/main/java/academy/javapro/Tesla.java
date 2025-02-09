ca30e000fa5fbb1e192db83976eb9773afd05459
Sean <82419183+MajenBej@users.noreply.github.com>
2025-02-09 04:56:00 +0000

    finished homework

--- src/main/java/academy/javapro/Tesla.java
+++ src/main/java/academy/javapro/Tesla.java
@@ -2,12 +2,90 @@ package academy.javapro;
 
 public class Tesla extends Vehicle implements Electric, Autonomous {
     private boolean autopilotEnabled;
+    @SuppressWarnings("unused")
     private boolean charging;
 
     // TODO: Create constructor that takes model and year
+    Tesla(String model, int year) {
+        super(model, year, model);
+        
+        this.autopilotEnabled = false;
+        this.charging = false;
+    }
+
+   
     // TODO: Set make to "Tesla"
+    
+    @Override
+    public void setMake(String make) {
+        super.setMake("Tesla");
+    }
+
+    public String getInfo() {
+        return this.make + " " + this.model + " ";
+    }
+    
     // TODO: Initialize autopilotEnabled and charging to false
+    
+
+    @Override
+    public void disableAutopilot() {
+        this.autopilotEnabled = false;
+    }
+
+    @Override
+    public void charge() {
+        // TODO Auto-generated method stub
+        System.out.println(this.getInfo() + " is now charging");
+        this.charging = true;
+    }
 
     // TODO: Implement all required methods from Vehicle, Electric, and Autonomous
+
+    @Override
+    public boolean isCharging() {
+        // TODO Auto-generated method stub
+        
+        return false;
+    }
+
+    
+    @Override
+    public void brake() {
+        // TODO Auto-generated method stub
+        System.out.println(getInfo() + "is braking...");
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println(getInfo() + "is accelerating...");
+    }
+
+    @Override
+    public void startEngine() {
+        // TODO Auto-generated method stub
+        this.isRunning = true;
+        this.disableAutopilot();
+        System.out.println(getInfo() + "started");
+    }
+
+    @Override
+    public void stopEngine() {
+        // TODO Auto-generated method stub
+        this.isRunning = false;
+        System.out.println(getInfo() + "stopped");
+        
+    }
+
+    @Override
+    public boolean isAutopilotEnabled() {
+        // TODO Auto-generated method stub
+       return this.autopilotEnabled;
+    }
+
+    @Override
+    public void enableAutopilot() {
+       this.autopilotEnabled = true;
+    }
     // Each method should include appropriate print statements
 }
--- src/main/java/academy/javapro/Vehicle.java
+++ src/main/java/academy/javapro/Vehicle.java
@@ -7,8 +7,49 @@ public abstract class Vehicle {
     protected boolean isRunning;
 
     // TODO: Add constructor
+    Vehicle(String model, int year, String make) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
 
+        this.isRunning = false;
+    }
     // TODO: Add abstract methods
+    public abstract void startEngine();
+        
+    public abstract void stopEngine();
+        
+    public abstract void accelerate();
 
+    public abstract void brake();
+        
     // TODO: Add getters/setters
+
+    public boolean isRunning() {
+        return this.isRunning;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
 }
